4f423c9c445f07289a5620c1d39a4ae32a6b081d
daveinoc <michael828inoc@gmail.com>
2022-05-12 13:17:47 +0800

    remove

--- src/ui/__tests__/Search.test.tsx
+++ src/ui/__tests__/Search.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Search } from "../Search";
 
 describe("Search Component", () => {
