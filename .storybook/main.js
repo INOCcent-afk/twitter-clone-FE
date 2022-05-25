aa6ffb4a6191eb7112ae5abe7166dd8ff85863ef
daveinoc <michael828inoc@gmail.com>
2022-05-26 01:05:28 +0800

    fix build and storybook error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -22,12 +22,15 @@ module.exports = {
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
   },
-  webpackFinal: async (config, { configType }) => {
-    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "../"),
-    };
+  webpackFinal: async (config) => {
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin(),
+    ];
 
     return config;
   },
--- src/stories/ui/Tweet.stories.tsx
+++ src/stories/ui/Tweet.stories.tsx
@@ -7,6 +7,14 @@ export default {
 
 export const Tweet = () => (
   <div className="w-full bg-black max-w-[600px]">
-    <TweetComponent />
+    <TweetComponent
+      text="My First Tweet!"
+      id={3}
+      createdAt="2022-04-02T16:56:59.867Z"
+      likes={3}
+      comments={0}
+      reshare={1}
+      author="Daveinoc"
+    />
   </div>
 );
